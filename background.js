8bf301f3038de5161db16d5368b696a55a0fbbe7
king-king <niuyiwangqun@sohu.com>
2018-01-22 15:48:45 +0800

    change

--- background.js
+++ background.js
@@ -1,6 +1,6 @@
 /*globals chrome*/
 chrome.runtime.onMessage.addListener((req) => {
     chrome.browserAction.setIcon({
-        path: req === 'enable' ? 'img/icon.png' : 'img/icon-alpha.png'
+        path: req === 'enable' ? 'img/16_icon.png' : 'img/16_icon-alpha.png'
     });
 });
